d29966c93f5d1e9f61008a4bd7e46305cd632692
Veeck <veeck@neofonie.de>
2014-04-04 18:14:23 +0200

    add css task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 // https://github.com/gulpjs/gulp
 var gulp    = require('gulp');
 var clean   = require('gulp-clean');
+var csso    = require('gulp-csso');
 
 // Clear the destination folder
 gulp.task('clean', function () {
@@ -9,11 +10,18 @@ gulp.task('clean', function () {
                .pipe(clean({ force: true }));
 });
 
-// Copy all application files into the 'dist' folder
-gulp.task('copy', function () {
-    return gulp.src(['src/**/*'])
+// Copy all application files into the 'dist' folder except user css
+gulp.task('copy', ['clean'], function () {
+    return gulp.src(['src/**/*', '!src/css/**/*.css'])
                .pipe(gulp.dest('dist'));
 });
 
+// Optimizes CSS files
+gulp.task('css', function () {
+    return gulp.src(['src/css/main.css', 'src/css/normalize.css'])
+               .pipe(csso())
+               .pipe(gulp.dest('dist/css'))
+});
+
 // The default task (called when you run `gulp`)
-gulp.task('default', ['clean', 'copy']);
+gulp.task('default', ['clean', 'copy', 'css']);
